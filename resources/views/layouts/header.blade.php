5b6f3117c4c7074cc6f824867fcb281f7c511ae1
alex hee <alex.hee87@gmail.com>
2016-12-27 15:05:24 +0800

    fix logout issues

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -45,6 +45,9 @@
                     @endif
                     <li><a href="#" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();"><i class="fa fa-sign-out fa-fw"></i> {!! trans('messages.logout') !!}</a>
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
                     </li>
                 </ul>
             </li>
